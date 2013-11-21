417b604962b98fd30c7e5746c150d00323398a08
Claudiu <claudiu@claudiupersoiu.ro>
2013-11-21 19:10:03 +0200

    new redesign

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ foreach($entityQuestions->getAll() as $question) {
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
 <title><?php echo TITLE; ?></title>
 <link rel="shortcut icon" href="images/favicon.ico"/>
 
@@ -51,9 +51,7 @@ foreach($entityQuestions->getAll() as $question) {
         min-height: 100%;
         height: 100%;
         font: bold Helvetica;
-        background-image: url('images/main.png');
-        background-position: right bottom;
-        background-repeat: no-repeat;
+
         margin:0;
         padding:0;
         font-family: 'Lucida Grande', 'Lucida Sans Unicode', Helvetica, Arial, sans-serif;
@@ -61,25 +59,68 @@ foreach($entityQuestions->getAll() as $question) {
 
     #answers p {
 
-        background-color: #fefba0;
+        background-color: #efefef;
         border: 1px solid #cccccc;
         cursor: pointer;
         background-position: left center;
         background-size: 17px 17px;
         -webkit-background-size: 17px 17px;
         -o-background-size: 17px 17px;
+        -webkit-border-radius: 5px;
+        border-radius: 5px;
+        background-clip: padding-box;
+
     }
 
     #answers p span {
         margin-left: 20px;
         display: block;
-        word-wrap:break-word;
+        padding: 10px 0;
     }
 
     #controls button {
-        width: 100%;
+        width: 50%;
         font-size: large;
         text-align: center;
+        -webkit-border-radius: 18px;
+        border-radius: 18px;
+        background-clip: padding-box;
+        border: 1px solid #427d93;
+        padding: 8px 0;
+        color: white;
+        float: left;
+        margin-right: 1%;
+
+        background-color: #6cbcda;
+        background-image: -webkit-gradient(linear, left top, left bottom, from(#6cbcda), to(#519cb8));
+        background-image: -webkit-linear-gradient(top, #6cbcda, #519cb8);
+        background-image:    -moz-linear-gradient(top, #6cbcda, #519cb8);
+        background-image:      -o-linear-gradient(top, #6cbcda, #519cb8);
+        background-image:         linear-gradient(to bottom, #6cbcda, #519cb8);
+        outline: none;
+    }
+
+    #controls button:active, #controls button:active:focus {
+        background-color: #519cb8;
+        background-image: -webkit-gradient(linear, left top, left bottom, from(#519cb8), to(#6cbcda));
+        background-image: -webkit-linear-gradient(top, #519cb8, #6cbcda);
+        background-image:    -moz-linear-gradient(top, #519cb8, #6cbcda);
+        background-image:      -o-linear-gradient(top, #519cb8, #6cbcda);
+        background-image:         linear-gradient(to bottom, #519cb8, #6cbcda);
+    }
+
+    #controls button:hover {
+        background-color: #519cb8;
+        background-image: -webkit-gradient(linear, left top, left bottom, from(#306f86), to(#4d91ab));
+        background-image: -webkit-linear-gradient(top, #306f86, #4d91ab);
+        background-image:    -moz-linear-gradient(top, #306f86, #4d91ab);
+        background-image:      -o-linear-gradient(top, #306f86, #4d91ab);
+        background-image:         linear-gradient(to bottom, #306f86, #4d91ab);
+    }
+
+    #controls #skip {
+        width: 49%;
+        margin-right: 0;
     }
 
     #controls #next {
@@ -87,52 +128,93 @@ foreach($entityQuestions->getAll() as $question) {
     }
 
     #answers p.selected {
-        background-color: #a4f7ff !important;
+        background-color: #666357 !important;
+        color: white;
     }
 
     #answers p.selected_correct {
-        background-color: #7ef3ff !important;
+        background-color: #70BF00 !important;
         background-image: url('images/ok.png');
         background-repeat: no-repeat;
+        color: white;
 
     }
 
     #answers p.error {
-        background-color: #fe909c !important;
+        background-color: #E54B17 !important;
         background-image: url('images/error.png');
         background-repeat: no-repeat;
+        color: white;
     }
 
     #answers p.correct {
-        background-color: #a0fea6 !important;
+        background-color: #70BF00 !important;
         background-image: url('images/correct.png');
         background-repeat: no-repeat;
+        color: white;
     }
 
     #results-stats {
         width: 100%;
+        display: none;
+        padding-top: 15px;
+        border-spacing:0;
+        clear: both;
+    }
+
+    #results-stats span {
+        text-align: center;
+        float: left;
+        font-size: 12px;
+        color: white;
+        padding: 1px 0px;
     }
 
     #results-stats #good-result-no {
-        background-color: #99fea4; width: 10%;
+        background-color: #70BF00;
+        width: 7%;
+        -moz-border-radius-bottomleft:5px;
+        -webkit-border-bottom-left-radius:5px;
+        border-bottom-left-radius:5px;
+        -moz-border-radius-topleft:5px;
+        -webkit-border-top-left-radius:5px;
+        border-top-left-radius:5px;
     }
 
     #results-stats #good-result {
-        background-color: #99fea4;
+        background-color: #70BF00;
+        display: inline-block;
     }
 
     #results-stats #bad-result {
-        background-color: #ff9396
+        background-color: #E54B17;
+        display: inline-block;
     }
 
     #results-stats #bad-result-no {
-        background-color: #ff9396;
-        text-align: right;
-        width: 10%;
+        background-color: #E54B17;
+        width: 7%;
+        -moz-border-radius-bottomright:5px;
+        -webkit-border-bottom-right-radius:5px;
+        border-bottom-right-radius:5px;
+        -moz-border-radius-topright:5px;
+        -webkit-border-top-right-radius:5px;
+        border-top-right-radius:5px;
     }
 
-    #timer {
+    #timer, #questions {
+        background: repeat-x scroll 0 0 #5a5a5a;
+        border-radius: 5px 5px 5px 5px;
+        color: white;
         float: left;
+        letter-spacing: 1px;
+        padding: 10px;
+
+    }
+
+    #question {
+        font-size: larger;
+        word-wrap:break-word;
     }
 
     #questions {
@@ -140,7 +222,17 @@ foreach($entityQuestions->getAll() as $question) {
     }
 
     h1 {
-        text-align: center; margin: 0 auto; width: 300px; font-size: large;
+        text-align: center; margin: 0 auto; font-size: 26px;
+    }
+
+    @media all and (max-width: 500px) {
+        h1 {
+            font-size: 18px;
+        }
+    }
+
+    h1 img {
+        display: inline; position: relative; vertical-align: middle; right: 5px;
     }
 
     #content {
@@ -148,7 +240,8 @@ foreach($entityQuestions->getAll() as $question) {
     }
 
     #header {
-        padding: 33px 5px 10px; background-repeat: repeat-x; background: url('images/top.gif');
+        padding: 33px 5px 10px;
+        background: url('images/top.gif') repeat-x top left;
     }
 
     #footer {
@@ -169,16 +262,45 @@ foreach($entityQuestions->getAll() as $question) {
         display: none;
     }
 
+    #final-results #results {
+        padding-bottom: 10px;
+    }
+
     #final-results .overlay {
         opacity: 0.6;width: 100%; height: 100%; background-color: #fff; position: absolute; top: 0; left:0; margin-left: 0px;
     }
 
     #final-results #result-board {
-        padding: 1%; position: absolute; top: 20%; width: 70%; left: 15%; text-align: center; background: #fefba0;border: 1px solid #cccccc;
+        position: absolute; top: 20%; width: 80%; left: 10%; text-align: center;
+    }
+
+    #result-board #container {
+        background: none repeat scroll 0px 0px rgb(241, 241, 241);
+        width: 100%; max-width: 500px;
+        margin: 0px auto; height: 100%;
+        border: 1px solid rgb(204, 204, 204);
+        padding: 1.5%;
+
+        -webkit-border-radius: 8px;
+        border-radius: 8px;
+        background-clip: padding-box;
     }
 
     #final-results #restart {
         width: 100%; font-size: large;
+        background-color: #6cbcda;
+        background-image: -webkit-gradient(linear, left top, left bottom, from(#6cbcda), to(#519cb8));
+        background-image: -webkit-linear-gradient(top, #6cbcda, #519cb8);
+        background-image:    -moz-linear-gradient(top, #6cbcda, #519cb8);
+        background-image:      -o-linear-gradient(top, #6cbcda, #519cb8);
+        background-image:         linear-gradient(to bottom, #6cbcda, #519cb8);
+        -webkit-border-radius: 18px;
+        border-radius: 18px;
+        background-clip: padding-box;
+        max-width: 300px;
+        border: 1px solid #427d93;
+        padding: 8px 0;
+        color: white;
     }
 
     .font-larder {
@@ -186,7 +308,10 @@ foreach($entityQuestions->getAll() as $question) {
     }
 
     #content #body {
-        padding: 5px 5px 60px 5px; opacity: 0.9; max-width: 900px; margin: 0 auto;
+        padding: 5px 5px 110px 5px;
+        opacity: 0.9;
+        max-width: 900px;
+        margin: 0 auto;
     }
 
 
@@ -225,6 +350,7 @@ foreach($entityQuestions->getAll() as $question) {
 
     var correct_answers = 0;
     var incorrect_answers = 0;
+    var skipped_questions = 0;
 
     var time = 0;
     var timer_container = null;
@@ -261,6 +387,7 @@ foreach($entityQuestions->getAll() as $question) {
             document.getElementById('final-results').style.display = 'block';
             document.getElementById('good-final-result').innerHTML = correct_answers;
             document.getElementById('bad-final-result').innerHTML = incorrect_answers;
+            document.getElementById('skipped-final-result').innerHTML = skipped_questions;
 
             document.getElementById('timer-result').innerHTML = timer_container.innerHTML;
             clearInterval(timer);
@@ -389,14 +516,21 @@ foreach($entityQuestions->getAll() as $question) {
         percent();
     }
 
+    function skippQuestion() {
+        skipped_questions++;
+        getQuestion();
+    }
+
     var percent = function () {
 
-        document.getElementById('good-result-no').innerHTML = correct_answers + ' ok';
-        document.getElementById('bad-result-no').innerHTML = incorrect_answers + ' bad';
+        document.getElementById('results-stats').style.display = 'block';
 
-        var proc = Math.round((80 / (correct_answers + incorrect_answers)) * correct_answers);
-        document.getElementById('good-result').width = proc + '%';
-        document.getElementById('bad-result').width = (80 - proc) + '%';
+        document.getElementById('good-result-no').innerHTML = correct_answers; // + ' ok';
+        document.getElementById('bad-result-no').innerHTML = incorrect_answers; // + ' bad';
+
+        var proc = Math.round((86 / (correct_answers + incorrect_answers)) * correct_answers);
+        document.getElementById('good-result').style.width = proc + '%';
+        document.getElementById('bad-result').style.width = (86 - proc) + '%';
     }
 
     window.onload = getQuestion;
@@ -410,39 +544,42 @@ foreach($entityQuestions->getAll() as $question) {
             <div id="timer">0:00:00</div>
 
             <div id="questions"></div>
-            <h1><?php echo TITLE; ?></h1>
+            <h1><img src="images/header-image.png"><?php echo TITLE; ?></h1>
         </div>
     </div>
     <div id="body">
 
         <div id="controls">
-            <button type="button" onclick="getQuestion();">next >>></button>
+
         </div>
         <p id="question"></p>
 
         <div id="answers" style="width: 100%;"></div>
 
         <div id="controls">
-            <button type="submit" id="verifica" onclick="checkAnswers();this.blur();">check answer</button>
-            <button type="button" id="next" onclick="getQuestion(); this.blur();">next >>></button>
-
+            <button type="submit" id="verifica" onclick="checkAnswers();this.blur();">check</button>
+            <button type="button" id="next" onclick="getQuestion(); this.blur();">continue</button>
+            <button type="button" id="skip" onclick="skippQuestion();">skip</button>
         </div>
-        <table id="results-stats">
-            <tr>
-                <td id="good-result-no"></td>
-                <td id="good-result"></td>
-                <td id="bad-result"></td>
-                <td id="bad-result-no"></td>
-            </tr>
-        </table>
+
+        <div id="results-stats">
+            <span id="good-result-no">ok</span>
+            <span id="good-result">&nbsp;</span>
+            <span id="bad-result">&nbsp;</span>
+            <span id="bad-result-no">bad</span>
+        </div>
         <div id="final-results">
             <div class="overlay"></div>
             <div id="result-board" class="font-larder">
-                <div class="font-larder">Results</div>
-                <div>Correct: <span id="good-final-result">x</span></div>
-                <div>Wrong: <span id="bad-final-result">x</span></div>
-                <div style="margin-bottom: 20px;">Time: <span id="timer-result"></span></div>
-                <div><button onclick="document.location = document.location;" id="restart">Restart</button></div>
+                <div id="container">
+                    <div class="font-larder" id='results'>Results</div>
+                    <div>Correct: <span id="good-final-result"></span></div>
+                    <div>Wrong: <span id="bad-final-result"></span></div>
+                    <div>Skipped: <span id="skipped-final-result"></span></div>
+                    <div style="margin-bottom: 20px;">Time: <span id="timer-result"></span></div>
+                    <div><button onclick="document.location = document.location;" id="restart">Restart</button></div>
+                </div>
+
             </div>
         </div>
 
