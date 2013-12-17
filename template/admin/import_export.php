ee9f70c04fe2fff254953f1967accb5b191c2d29
Claudiu <claudiu@claudiupersoiu.ro>
2013-12-17 19:26:34 +0200

    added project url to file headers

--- template/admin/import_export.php
+++ template/admin/import_export.php
@@ -4,7 +4,11 @@
  * Copyright (c) 2013 Claudiu Persoiu (http://www.claudiupersoiu.ro/)
  *
  * This file is part of "Just quizzing".
-
+ *
+ * Official project page: http://blog.claudiupersoiu.ro/just-quizzing/
+ *
+ * You can download the latest version from https://github.com/claudiu-persoiu/Just-Quizzing
+ *
  * "Just quizzing" is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
